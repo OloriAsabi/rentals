a954e33e474e7b6cf4dd0ebd09d49658c1c45c1a
OloriAsabi <developerqueenlatifa@gmail.com>
2022-03-24 16:22:56 +0100

    build

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    loader: 'imgix',
+    path:"/",
     domains: ['raw.githubusercontent.com',
     'bayut-production.s3.eu-central-1.amazonaws.com'],
   }
